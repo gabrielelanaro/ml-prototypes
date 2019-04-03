8f4292df46ad095a5b104bc2cfedeeef5b9547d9
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2019-04-03 08:37:51 -0700

    stop bthering me

--- prototypes/styletransfer/model.py
+++ prototypes/styletransfer/model.py
@@ -5,13 +5,17 @@ import time
 
 from typing import NamedTuple, Tuple, List
 
+import tensorflow.contrib.eager as tfe
+
 from tensorflow.python.keras import models
 from tensorflow.python.keras import losses
 from tensorflow.python.keras import layers
 from tensorflow.python.keras import backend as K
 import tensorflow.contrib.eager as tfe
 
-from .images import process_vgg_batch
+from .images import process_vgg_batch, deprocess_vgg
+
+tf.enable_eager_execution()
 
 
 class StyleTransferResult(NamedTuple):
@@ -209,65 +213,54 @@ class StyleTransfer:
 
         # Get the style and content feature representations (from our specified intermediate layers)
         style_features, content_features = self.feature_representations(
-            model, content_path, style_path
+            content_img, style_img
         )
         gram_style_features = [
             gram_matrix(style_feature) for style_feature in style_features
         ]
 
         # Set initial image
-        init_image = load_and_process_img(content_path)
-        init_image = tfe.Variable(init_image, dtype=tf.float32)
+        init_image = tfe.Variable(content_img, dtype=tf.float32)
+
         # Create our optimizer
         opt = tf.train.AdamOptimizer(learning_rate=5, beta1=0.99, epsilon=1e-1)
-
-        # For displaying intermediate images
-        iter_count = 1
 
         # Store our best result
         best_loss, best_img = float("inf"), None
 
         # Create a nice config
         loss_weights = (style_weight, content_weight)
-        cfg = {
-            "model": model,
-            "loss_weights": loss_weights,
-            "init_image": init_image,
-            "gram_style_features": gram_style_features,
-            "content_features": content_features,
-        }
 
         # For displaying
         num_rows = 2
         num_cols = 5
         display_interval = num_iterations / (num_rows * num_cols)
         start_time = time.time()
-        global_start = time.time()
 
         norm_means = np.array([103.939, 116.779, 123.68])
         min_vals = -norm_means
         max_vals = 255 - norm_means
 
-        imgs = []
         for i in range(num_iterations):
-            grads, all_loss = compute_grads(**cfg)
+            grads, all_loss = self._loss_gradient(
+                loss_weights, init_image, gram_style_features, content_features
+            )
             loss, style_score, content_score = all_loss
             opt.apply_gradients([(grads, init_image)])
             clipped = tf.clip_by_value(init_image, min_vals, max_vals)
             init_image.assign(clipped)
-            end_time = time.time()
 
             if loss < best_loss:
                 # Update best loss and best image from total loss.
                 best_loss = loss
-                best_img = deprocess_img(init_image.numpy())
+                best_img = deprocess_vgg(init_image.numpy())
 
             if i % display_interval == 0:
                 start_time = time.time()
 
             # Use the .numpy() method to get the concrete numpy array
             plot_img = init_image.numpy()
-            plot_img = deprocess_img(plot_img)
+            plot_img = deprocess_vgg(plot_img)
 
             yield StyleTransferResult(
                 image=plot_img,
@@ -278,11 +271,9 @@ class StyleTransfer:
                 elapsed_time_sec=time.time() - start_time,
             )
 
-        return best_img, best_loss
-
     def _process_img_batch(self, img_batch):
         # Takes a numpy image and makes it into an image processed to be ready for vgg
-        return tf.convert_to_tensor(process_vgg(img_batch), tf.float32)
+        return tf.convert_to_tensor(process_vgg_batch(img_batch), tf.float32)
 
     def _compute_loss(
         self,
--- prototypes/styletransfer/tests/test_model.py
+++ prototypes/styletransfer/tests/test_model.py
@@ -1,6 +1,8 @@
 import numpy as np
 import tensorflow as tf
-from prototypes.styletransfer.model import StyleTransfer
+from prototypes.styletransfer.model import StyleTransfer, gram_matrix
+import tensorflow.contrib.eager as tfe
+
 
 rng = np.random.RandomState(42)
 
@@ -12,22 +14,46 @@ def _sample_img(size):
 def test_model():
     content_img = _sample_img(512)
     style_img = _sample_img(512)
-    init_img = _sample_img(512)
-   
+
     model = StyleTransfer()
 
     style_rep, content_rep = model.feature_representations(content_img, style_img)
-    
-    assert isinstance(content_rep, List)
-    assert isinstance(style_rep, List)
-    
+
+    assert isinstance(content_rep, list)
+    assert isinstance(style_rep, list)
+
+
+def test_model_loss():
+    # NOTE: you're not supposed to test private methods but those are
+    # complicated private methods that justify their usage. We could
+    # also refactor to make more things public and testable.
+    content_img = _sample_img(512)
+    style_img = _sample_img(512)
+    init_img = _sample_img(512)
+
+    model = StyleTransfer()
+
+    content_rep, style_rep = model.feature_representations(content_img, style_img)
+
     init_img = model._process_img_batch(init_img)
     init_img = tfe.Variable(init_img, dtype=tf.float32)
-    
+
     gram_style_features = [gram_matrix(style_feature) for style_feature in style_rep]
     loss_weights = (0.5, 0.5)
-    losses = model._compute_loss(loss_weights, init_img, gram_style_features, content_rep)
-    
-    assert isinstance(losses, Tuple)
+    losses = model._compute_loss(
+        loss_weights, init_img, gram_style_features, content_rep
+    )
+
+    assert isinstance(losses, tuple)
     assert isinstance(losses[0], tf.Tensor)
 
+
+def test_run_styletransfer():
+    content_img = _sample_img(512)
+    style_img = _sample_img(512)
+
+    model = StyleTransfer()
+    content_rep, style_rep = model.feature_representations(content_img, style_img)
+
+    for st in model.run_style_transfer(content_img, style_img, num_iterations=10):
+        assert isinstance(st.image, np.ndarray)
