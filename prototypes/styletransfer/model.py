f68892c19a59a8b1a4d1c578905f80050a57c991
FraPochetti <fra.pochetti@gmail.com>
2019-04-03 15:19:21 +0200

    added global loss function and fixed couple of issues

--- prototypes/styletransfer/model.py
+++ prototypes/styletransfer/model.py
@@ -2,12 +2,13 @@
 import tensorflow as tf
 import numpy as np
 
-from typing import Tuple
+from typing import Tuple, List
 
 from tensorflow.python.keras import models
 from tensorflow.python.keras import losses
 from tensorflow.python.keras import layers
 from tensorflow.python.keras import backend as K
+import tensorflow.contrib.eager as tfe
 
 from .images import process_vgg_batch
 
@@ -67,10 +68,10 @@ class StyleTransfer:
     ) -> Tuple[tf.Tensor, tf.Tensor]:
 
         content_rep, style_rep = self.feature_representations_batch(
-            np.expand_dims(content_img, 0), np.expand_dims(style_img, 0)
+           content_img, style_img
         )
 
-        return content_rep[0], style_rep[0]
+        return content_rep, style_rep
 
     def feature_representations_batch(
         self, content_img_batch: np.array, style_img_batch: np.array
@@ -89,7 +90,7 @@ class StyleTransfer:
         Returns:
             returns the style features and the content features. 
         """
-
+        
         content_img_batch = self._process_img_batch(content_img_batch)
         style_img_batch = self._process_img_batch(style_img_batch)
 
@@ -109,7 +110,60 @@ class StyleTransfer:
 
     def _process_img_batch(self, img_batch):
         # Takes a numpy image and makes it into an image processed to be ready for vgg
-        return tf.convert_to_tensor(process_vgg_batch(img_batch), tf.float32)
+        return tf.convert_to_tensor(process_vgg(img_batch), tf.float32)
+
+    def _compute_loss(
+        self, loss_weights: List, init_image: tfe.Variable, gram_style_features: List, content_features: List
+        ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+      """This function will compute the loss total loss.
+    
+      Arguments:
+        loss_weights: The weights of each contribution of each loss function. 
+          (style weight, content weight)
+        init_image: Our initial base image. This image is what we are updating with 
+          our optimization process. We apply the gradients wrt the loss we are 
+          calculating to this image.
+        gram_style_features: Precomputed gram matrices corresponding to the 
+          defined style layers of interest.
+        content_features: Precomputed outputs from defined content layers of 
+          interest.
+
+      Returns:
+        returns the total loss, style loss, content loss
+      """
+      style_weight, content_weight = loss_weights
+    
+      # Feed our init image through our model. This will give us the content and 
+      # style representations at our desired layers. Since we're using eager
+      # our model is callable just like any other function!
+      model_outputs = self._model(init_image)
+
+      num_style_layers = len(self._style_layers)
+      num_content_layers = len(self._content_layers)
+
+      style_output_features = model_outputs[:num_style_layers]
+      content_output_features = model_outputs[num_style_layers:]
+    
+      style_score = 0
+      content_score = 0
+
+      # Accumulate style losses from all layers
+      # Here, we equally weight each contribution of each loss layer
+      weight_per_style_layer = 1.0 / float(num_style_layers)
+      for target_style, comb_style in zip(gram_style_features, style_output_features):
+        style_score += weight_per_style_layer * self.get_style_loss(comb_style[0], target_style)
+
+      # Accumulate content losses from all layers 
+      weight_per_content_layer = 1.0 / float(num_content_layers)
+      for target_content, comb_content in zip(content_features, content_output_features):
+        content_score += weight_per_content_layer* self.get_content_loss(comb_content[0], target_content)
+    
+      style_score *= style_weight
+      content_score *= content_weight
+
+      # Get total loss
+      loss = style_score + content_score 
+      return loss, style_score, content_score
 
 
 def gram_matrix(input_tensor: tf.Tensor) -> tf.Tensor:
@@ -121,4 +175,3 @@ def gram_matrix(input_tensor: tf.Tensor) -> tf.Tensor:
 
     # TODO: why do you divide by n? It could be the weighting factor
     return gram / tf.cast(n, tf.float32)
-
