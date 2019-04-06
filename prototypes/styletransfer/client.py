69836649dcc2840732ebc069242c0945374609e0
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2019-04-06 13:58:52 -0700

    FIrst websocket prototype

---
+++ prototypes/styletransfer/client.py
@@ -0,0 +1,38 @@
+"""Websocket client for testing"""
+
+from tornado.websocket import websocket_connect
+from tornado.ioloop import IOLoop
+from .server import State
+import json
+
+
+class WebsocketClient:
+    """Websocket client for testing our server"""
+
+    def __init__(self, url: str):
+        self.url = url
+
+    async def connect(self):
+        self.ws = await websocket_connect(self.url)
+
+        while True:
+            msg = await self.ws.read_message()
+            print("Message received from server:\n#{}#".format(msg))
+            if msg is None:
+                # Con nection is closed
+                print("Connection is closed")
+                break
+
+            msg = json.loads(msg)
+            if msg["state"] == State.MODEL_LOADED.value:
+                # TODO: send a random image
+                await self.ws.write_message(json.dumps({"action": "request_image"}))
+            else:
+                print("MSG IS {}".format(msg["state"]))
+
+
+if __name__ == "__main__":
+    loop = IOLoop.instance()
+    client = WebsocketClient("ws://localhost:8000/styletransfer")
+    loop.spawn_callback(client.connect)
+    loop.start()
---
+++ prototypes/styletransfer/server.py
@@ -0,0 +1,102 @@
+from tornado.websocket import WebSocketHandler
+from tornado.ioloop import IOLoop
+
+from .model import StyleTransfer, StyleTransferResult
+import time
+import enum
+import json
+
+import numpy as np
+
+
+class InvalidTransition(Exception):
+    pass
+
+
+class State(enum.Enum):
+    INIT = "init"
+    MODEL_LOADING = "model_loading"
+    MODEL_LOADED = "model_loaded"
+    START_ITERATION = "start_iteration"
+    END_ITERATION = "end_iteration"
+    END = "end"
+
+
+class StyleTransferController:
+    def __init__(self, ws: WebSocketHandler):
+        self._ws = ws
+        self.state = State.INIT
+        self._model = None
+
+    # Transitions
+    async def load_model(self):
+        if self.state != State.INIT:
+            raise InvalidTransition()
+
+        await self._set_and_send_state(State.MODEL_LOADING)
+
+        # We run model initialization in a separate thread so that
+        # we don't block the tornado event loop.
+        loop = IOLoop.current()
+        self._model: StyleTransfer = await loop.run_in_executor(
+            None, self._do_load_model
+        )
+
+        await self._set_and_send_state(State.MODEL_LOADED)
+
+    async def request_image(self, img):
+        if self.state != State.MODEL_LOADED:
+            raise InvalidTransition()
+
+        # We can't run a for loop because we risk of blocking everything
+        # TODO: this could be converted to an async for loop by creating a function
+        # async def iterate_in_executor(executor):
+        #     pass
+        loop = IOLoop.current()
+        iterator = iter(self._model.run_style_transfer(img, img, num_iterations=10))
+        while True:
+            await self._set_and_send_state(State.START_ITERATION)
+
+            style_results: StyleTransferResult = await loop.run_in_executor(
+                None, next, iterator
+            )
+
+            await self._set_and_send_state(
+                State.END_ITERATION, {"iteration": style_results.iteration_no}
+            )
+
+    async def _set_and_send_state(self, state: State, data=None):
+        self.state = state
+        await self._ws.write_message({"state": self.state.value, "data": data})
+
+    def _do_load_model(self) -> StyleTransfer:
+        return StyleTransfer()
+
+
+class StyleTransferSocket(WebSocketHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._controller = StyleTransferController(self)
+
+    async def open(self):
+        # TODO: We load the model once the connection is open
+        # an alternative would be to make the websocket load the model later on.
+        await self._controller.load_model()
+
+    async def on_message(self, message):
+        print("Received message: {}".format(message))
+
+        message = json.loads(message)
+
+        if message["action"] == "request_image":
+            # TODO: Implement parsing code
+            # img = _parse_image(message["image"])
+            img = np.random.randint(0, 255, (512, 512, 3)).astype("float32")
+            await self._controller.request_image(img)
+        else:
+            raise Exception("invalid action")
+
+    def on_close(self):
+        # TODO: maybe add some logging here, and devise some way to stop the
+        # iterations maybe
+        pass
