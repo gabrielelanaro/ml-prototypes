ed17e911b0c911e7d918c1bdaf95b18ac28cc8e5
FraPochetti <fra.pochetti@gmail.com>
2019-04-09 18:42:24 +0200

    adding statis website and modifying lambda

--- prototypes/styletransfer/lambda.py
+++ prototypes/styletransfer/lambda.py
@@ -1,36 +1,54 @@
-import json
-import boto3
+import json, boto3
 
 REGION = 'eu-west-1' # region to launch instance.
-AMI = 'ami-047bb4163c506cd98' # Amazon Linux AMI as per https://aws.amazon.com/amazon-linux-ami/
+AMI = "ami-0a961c5be1838f98e" # our custom AMI
 INSTANCE_TYPE = 'p2.xlarge' # instance type to launch.
 
 EC2 = boto3.client('ec2', region_name=REGION)
 
+def format_response(message, status_code):
+    return {
+        'statusCode': str(status_code),
+        'body': json.dumps(message),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+            }
+        }
+
 def lambda_handler(event, context):
 
     body = json.loads(event['body'])
-    message = body['data']
-
-    # run 2 commands, then shut instance down after 2 minute
-    init_script = """#!/bin/bash
-    yum update -y
-    yum install -y httpd24
-    shutdown -h +2"""
-
+  
+    init_script = f"""#!/bin/bash
+    cd /home/ubuntu
+    git clone https://github.com/gabrielelanaro/ml-prototypes.git
+    source activate tensorflow_p36
+    cd ml-prototypes
+    python -m prototypes.styletransfer.app --address=0.0.0.0 --port=8000
+    shutdown -h +10
+    """
+    
     instance = EC2.run_instances(
         ImageId=AMI,
         InstanceType=INSTANCE_TYPE,
         MinCount=1, # required by boto, even though it's kinda obvious.
         MaxCount=1,
+        KeyName="FraDeepLearn",
         InstanceInitiatedShutdownBehavior='terminate', # make shutdown in script terminate ec2
         UserData=init_script # file to run on instance init.
     )
 
     instance_id = instance['Instances'][0]['InstanceId']
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps({"data": message,
-                           "instance_id": instance_id})
-    }
+    #inst = boto3.resource('ec2', region_name=REGION).Instance(instance_id)
+
+    # Wait for the instance to enter the running state
+    #inst.wait_until_running()
+
+    # Reload the instance attributes
+    #inst.load()
+    #print(inst.public_dns_name)
+    #print(type(inst.public_dns_name))
+
+    return format_response(f"instance_id: {instance_id}", 200)
