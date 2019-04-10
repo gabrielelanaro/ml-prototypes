1b690cf66205d152cc7f955755ba198aa09b8ad2
Francesco Pochetti <fra.pochetti@gmail.com>
2019-04-10 22:58:25 +0200

    added instance dns and gpu logic (#17)

--- prototypes/styletransfer/lambda.py
+++ prototypes/styletransfer/lambda.py
@@ -1,4 +1,5 @@
-import json, boto3
+import json, boto3, botocore
+
 # This file is to store a lambda handler which gets triggered by an Amazon API Gateway POST method.  
 # A lambda handler is a serverless python function executed each time the trigger is pulled. 
 # In this specific case, the function does the following:
@@ -6,12 +7,13 @@ import json, boto3
 # 2) clones our transfer learning repo
 # 3) launches the server side websocket
 # 4) kills the EC2 after 10 minutes
-# 5) returns to the browser the instance_id of the spinned up EC2 instance  
+# 5) returns to the browser the pulic DNS of the spinned up EC2 instance  
 
 REGION = 'eu-west-1' # region to launch instance.
 AMI = "ami-0a961c5be1838f98e" # our custom AMI
-INSTANCE_TYPE = 'p2.xlarge' # instance type to launch.
-
+available_instances = ["g2.2xlarge", "g2.8xlarge", "g3.16xlarge", "g3.4xlarge", "g3.8xlarge",
+                       "g3s.xlarge", "p2.8xlarge", "p2.xlarge", "p3.2xlarge", "p3.8xlarge"]
+                       
 EC2 = boto3.client('ec2', region_name=REGION)
 
 def format_response(message, status_code):
@@ -24,6 +26,26 @@ def format_response(message, status_code):
             }
         }
 
+     
+def spin_up_ec2(ec2_object, instance_type, script):
+    
+    try:
+        instance = ec2_object.run_instances(
+            ImageId=AMI,
+            InstanceType=instance_type,
+            MinCount=1, 
+            MaxCount=1,
+            KeyName="FraDeepLearn",
+            InstanceInitiatedShutdownBehavior='terminate', # make shutdown in script terminate ec2
+            UserData=script # file to run on instance init.
+        )
+        
+    except botocore.exceptions.ClientError:
+        instance = None
+    
+    return instance
+    
+
 def lambda_handler(event, context):
 
     body = json.loads(event['body'])
@@ -33,31 +55,22 @@ def lambda_handler(event, context):
     git clone https://github.com/gabrielelanaro/ml-prototypes.git
     source activate tensorflow_p36
     cd ml-prototypes
+    shutdown -h +3
     python -m prototypes.styletransfer.app --address=0.0.0.0 --port=8000
-    shutdown -h +10
+    shutdown -h now
     """
-    
-    instance = EC2.run_instances(
-        ImageId=AMI,
-        InstanceType=INSTANCE_TYPE,
-        MinCount=1, # required by boto, even though it's kinda obvious.
-        MaxCount=1,
-        KeyName="FraDeepLearn",
-        InstanceInitiatedShutdownBehavior='terminate', # make shutdown in script terminate ec2
-        UserData=init_script # file to run on instance init.
-    )
+    for INSTANCE_TYPE in available_instances:
+        instance = spin_up_ec2(EC2, INSTANCE_TYPE, init_script)
+        if instance:
+            instance_id = instance['Instances'][0]['InstanceId']
+            break
+        
+    if not instance: 
+        return format_response(f"id: 'limit exceeded', type: 'limit exceeded', dns: 'limit exceeded'", 200) 
 
-    instance_id = instance['Instances'][0]['InstanceId']
+    inst = boto3.resource('ec2', region_name=REGION).Instance(instance_id)
+    inst.wait_until_running()
+    inst.load()
+    public_dns = inst.public_dns_name
 
-    # Lines 58-61 are used to retrieve the public DNS of the EC2 instance
-    # Lambda has spun up. This attribute is made available only when the 
-    # machine switches to its RUNNING state, so not at boot time.
-    # This section is commented as even if the code works as intented,
-    # for unclear reasons, Lambda does not return the expected output
-
-    #inst = boto3.resource('ec2', region_name=REGION).Instance(instance_id)
-    #inst.wait_until_running()
-    #inst.load()
-    #public_dns = inst.public_dns_name
-
-    return format_response(f"instance_id: {instance_id}", 200)
+    return format_response(f"id: {instance_id}, type: {INSTANCE_TYPE}, dns: {public_dns}", 200)
