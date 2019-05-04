f509d9ae0c194949a12fca3a902014addd07192c
FraPochetti <fra.pochetti@gmail.com>
2019-05-04 10:22:17 +0200

    giving users abilty to upload video to S3 (+ lambda)

---
+++ prototypes/styletransfer/st_video_lambda.py
@@ -0,0 +1,80 @@
+import boto3, json
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+    
+    s3 = boto3.client('s3')
+    
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key']
+    
+    response = s3.head_object(Bucket=bucket, Key=key)
+
+    print(bucket)
+    print(key)
+    print(response['Metadata']['email'])
+    
+    SENDER = "fra.pochetti@gmail.com"
+    RECIPIENT = "fra.pochetti@gmail.com"
+    SUBJECT = "Francesco Email Test"
+    
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                 "This email was sent with Amazon SES using the "
+                 "AWS SDK for Python (Boto)."
+                )
+
+    # The HTML body of the email.
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+      <h1>Amazon SES Test (SDK for Python)</h1>
+      <p>This email was sent with
+        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+        <a href='https://aws.amazon.com/sdk-for-python/'>
+          AWS SDK for Python (Boto)</a>.</p>
+    </body>
+    </html>
+                """            
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses')
+
+    # Try to send the email.
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
+    
+    return
