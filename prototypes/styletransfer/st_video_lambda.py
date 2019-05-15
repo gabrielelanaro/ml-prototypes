4f4fab68f2eef694f23603376bce04b6fea8a705
FraPochetti <fra.pochetti@gmail.com>
2019-05-15 17:05:32 +0200

    refactoring video lambda for some error handling

--- prototypes/styletransfer/st_video_lambda.py
+++ prototypes/styletransfer/st_video_lambda.py
@@ -1,61 +1,41 @@
 import boto3, json
 from botocore.exceptions import ClientError
 
-def lambda_handler(event, context):
-    
-    s3 = boto3.client('s3')
-    
-    record = event['Records'][0]
-    bucket = record['s3']['bucket']['name']
-    key = record['s3']['object']['key']
-    
-    response = s3.head_object(Bucket=bucket, Key=key)
-    email = response['Metadata']['email']
-    style = response['Metadata']['style']
+# VARIABLES
+AWS_REGION = "eu-west-1"
+AMI = "ami-0ac4506ffe115b721"
+available_instances = ["p2.xlarge"]
+SENDER = "fra.pochetti@gmail.com"
+CHARSET = "UTF-8"
 
-    print(bucket)
-    print(key)
-    print(email)
-    print(style)
-    
-    SENDER = "fra.pochetti@gmail.com"
-    RECIPIENT = email
-    AWS_REGION = "eu-west-1"
-    SUBJECT = "VisualNeurons.com - your GIF has been ingested!"
-    
-    BODY_TEXT = ("VisualNeurons.com - your GIF has been ingested! \r\n"
-                 "The purpose of this email is to confirm that we successfully ingested your file"
-                 "and that we are currently processing it."
-                )
+EC2 = boto3.client("ec2", region_name=AWS_REGION)
+s3 = boto3.client('s3')
+client = boto3.client('ses', region_name=AWS_REGION)
 
-    # The HTML body of the email.
-    BODY_HTML = """
-    <html>
+def spin_up_ec2(ec2_object, instance_type, script):
 
-    <head></head>
-
-    <body>
-        <h3>You just uploaded a GIF to S3. Congratulations!</h3>
-        <p>The purpose of this email is to confirm that we successfully ingested your file and that we are currently processing it.
-            <br>
-            When our GPUs finish crunching your request, you will receive another email with the link to your Style-Transferred GIF.
-            <br>
-            This will happen in a hour or so. Thanks for your patience!
-        </p>
-    </body>
-
-    </html>
-                """            
-
-    # The character encoding for the email.
-    CHARSET = "UTF-8"
-
-    # Create a new SES resource and specify a region.
-    client = boto3.client('ses', region_name=AWS_REGION)
-
-    # Try to send the email.
     try:
-        #Provide the contents of the email.
+        instance = ec2_object.run_instances(
+            ImageId=AMI,
+            InstanceType=instance_type,
+            MinCount=1,
+            MaxCount=1,
+            KeyName="GabKP",
+            SecurityGroups=[
+                "transfer-learning",
+            ],
+            IamInstanceProfile={"Arn": "arn:aws:iam::257446244580:role/TransferLearningEC2Role"}, # this is needed to give EC2 the right permissions
+            InstanceInitiatedShutdownBehavior='terminate', # make shutdown in script terminate ec2
+            UserData=script # file to run on instance init.
+        )
+
+    except ClientError:
+        instance = None
+
+    return instance
+
+def send_email_to_user(RECIPIENT, SUBJECT, BODY_TEXT, BODY_HTML):
+    try:
         response = client.send_email(
             Destination={
                 'ToAddresses': [
@@ -88,3 +68,81 @@ def lambda_handler(event, context):
         print(response['MessageId'])
     
     return
+
+def lambda_handler(event, context):
+    
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key']
+    
+    response = s3.head_object(Bucket=bucket, Key=key)
+    email = response['Metadata']['email']
+    RECIPIENT = email
+    style = response['Metadata']['style']
+
+    print(bucket)
+    print(key)
+    print(email)
+    print(style)
+
+    ###################################
+    # init_script IS THE CORE PART WHICH NEEDS TO BE FILLED! 
+    ###################################
+    init_script = f"""#!/bin/bash
+cd /home/ubuntu
+cd ml-prototypes
+git pull
+    """
+    
+    for INSTANCE_TYPE in available_instances:
+        instance = spin_up_ec2(EC2, INSTANCE_TYPE, init_script)
+        if instance:
+            SUBJECT = "VisualNeurons.com - your GIF has been ingested!"
+
+            BODY_TEXT = ("VisualNeurons.com - your GIF has been ingested! \r\n"
+                         "The purpose of this email is to confirm that we successfully ingested your file"
+                         "and that we are currently processing it."
+                        )
+
+            BODY_HTML = """
+            <html>
+
+            <head></head>
+
+            <body>
+                <h4>You just uploaded a GIF to S3. Congratulations!</h4>
+                <p>The purpose of this email is to confirm that we successfully ingested your file and that we are currently processing it.
+                    <br>
+                    When our GPUs finish crunching your request, you will receive another email with the link to your Style-Transferred GIF.
+                    <br>
+                    This will happen in a hour or so. Thanks for your patience!
+                </p>
+            </body>
+
+            </html>
+                        """
+            send_email_to_user(RECIPIENT, SUBJECT, BODY_TEXT, BODY_HTML)
+            return
+
+    if not instance:
+        SUBJECT = "VisualNeurons.com - Can you please re-try later?"
+        BODY_TEXT = ("VisualNeurons.com - Can you please re-try later? \r\n"
+                     "Ouch... Sorry, it seems we ran out of artistic GPUs! Can you try again later?"
+                    )
+        
+        BODY_HTML = """
+        <html>
+        <head></head>
+        <body>
+            <h4>:( We cannot process your request...</h4>
+            <p>Ouch... Sorry, it seems we ran out of artistic GPUs! Can you try again later?
+                <br>
+                We have a limited number of EC2 instances we can run in parallel.
+                <br>
+                As now we are at full capacity, we cannot process your request. Sorry about this!
+            </p>
+        </body>
+        </html>
+                    """
+        send_email_to_user(RECIPIENT, SUBJECT, BODY_TEXT, BODY_HTML)
+        return
